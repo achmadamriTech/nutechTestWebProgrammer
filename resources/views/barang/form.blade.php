d0b416dd01381fcfcbb8c3028eb6cb4b2d8eb82c
unknown <nyamukterbang8@gmail.com>
2021-06-15 19:19:31 +0700

    Merubah Form Edit Barang

--- resources/views/barang/form.blade.php
+++ resources/views/barang/form.blade.php
@@ -13,63 +13,7 @@
 
 <div class="content">
     <div class="container">
-    <div class="row">
-            <div class="col-md-3 col-sm-6 col-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
-
-                    <div class="info-box-content">
-                        <span class="info-box-text">Jumlah Barang</span>
-                        <span class="info-box-number">1,410</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-3 col-sm-6 col-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
-
-                    <div class="info-box-content">
-                        <span class="info-box-text">Barang Termahal</span>
-                        <span class="info-box-number">410</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-3 col-sm-6 col-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
-
-                    <div class="info-box-content">
-                        <span class="info-box-text">Barang Termurah</span>
-                        <span class="info-box-number">13,648</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-3 col-sm-6 col-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
-
-                    <div class="info-box-content">
-                        <span class="info-box-text">Stok Terendah</span>
-                        <span class="info-box-number">93,139</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
-
-        <div class="row">
+            <div class="row">
         <div class="button-add">
                 <a  href="{{route('barang')}}" class="btn btn-danger"><i class="fas fa-arrow-left"></i> Back</a>
             </div>
